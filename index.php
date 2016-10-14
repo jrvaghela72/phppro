5a5ac92f8223ebf5979bcea81b3753111dd0168d
DESKTOP-GD9FLJA\jrvag <jrvaghela72@gmail.com>
2016-10-14 18:33:54 +0530

    this is first comment after start work in phpstrome

--- index.php
+++ index.php
@@ -12,37 +12,55 @@
 
 <!--NAV content-->
 <!-- Always shows a header, even in smaller screens. -->
-<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
+<!-- Uses a header that contracts as the page scrolls down. -->
+<style>
+    .demo-layout-waterfall .mdl-layout__header-row .mdl-navigation__link:last-of-type {
+        padding-right: 0;
+    }
+</style>
+
+<div class="demo-layout-waterfall mdl-layout mdl-js-layout">
+    <header class="mdl-layout__header mdl-layout__header--waterfall">
+        <!-- Top row, always visible -->
         <div class="mdl-layout__header-row">
             <!-- Title -->
             <span class="mdl-layout-title">Title</span>
-            <!-- Add spacer, to align navigation to the right -->
             <div class="mdl-layout-spacer"></div>
-            <!-- Navigation. We hide it in small screens. -->
-            <nav class="mdl-navigation mdl-layout--large-screen-only">
-                <a class="mdl-navigation__link" href="">HOME</a>
-                <a class="mdl-navigation__link" href="">ABOUT</a>
-                <a class="mdl-navigation__link" href="">CONTACT</a>
-                <a class="mdl-navigation__link" href="">DROPDOWN</a>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+                  mdl-textfield--floating-label mdl-textfield--align-right">
+                <label class="mdl-button mdl-js-button mdl-button--icon"
+                       for="waterfall-exp">
+                    <i class="material-icons">search</i>
+                </label>
+                <div class="mdl-textfield__expandable-holder">
+                    <input class="mdl-textfield__input" type="text" name="sample"
+                           id="waterfall-exp">
+                </div>
+            </div>
+        </div>
+        <!-- Bottom row, not visible on scroll -->
+        <div class="mdl-layout__header-row">
+            <div class="mdl-layout-spacer"></div>
+            <!-- Navigation -->
+            <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
             </nav>
         </div>
     </header>
     <div class="mdl-layout__drawer">
         <span class="mdl-layout-title">Title</span>
         <nav class="mdl-navigation">
-            <a class="mdl-navigation__link" href="">ONE</a>
-            <a class="mdl-navigation__link" href="">TWO</a>
-            <a class="mdl-navigation__link" href="">THREE</a>
-            <a class="mdl-navigation__link" href="">FOUR</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
         </nav>
     </div>
     <main class="mdl-layout__content">
-        <div class="page-content">
-            <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
-                <i class="material-icons">add</i>
-            </button>
-        </div>
+        <div class="page-content"><!-- Your content goes here --></div>
     </main>
 </div>
 
--- script/script.js
+++
@@ -1,6 +0,0 @@
-var button = document.createElement('button');
-var textNode = document.createTextNode('Click Me!');
-button.appendChild(textNode);
-button.className = 'mdl-button mdl-js-button mdl-js-ripple-effect';
-componentHandler.upgradeElement(button);
-document.getElementById('container').appendChild(button);
